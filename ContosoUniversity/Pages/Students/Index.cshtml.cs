c3aeb502a595c95dbcb967ff8edfeafbebc37eed
Bartosz <szabon.bartosz@gmail.com>
2018-07-20 13:44:23 +0200

    added search functionality

--- ContosoUniversity/Pages/Students/Index.cshtml.cs
+++ ContosoUniversity/Pages/Students/Index.cshtml.cs
@@ -18,11 +18,44 @@ namespace ContosoUniversity.Pages.Students
             _context = context;
         }
 
+        public string NameSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
+
         public IList<Student> Student { get;set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder, string searchString)
         {
-            Student = await _context.Student.ToListAsync();
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+            CurrentFilter = searchString;
+
+            IQueryable<Student> studentIQ = from s in _context.Student
+                                            select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                studentIQ = studentIQ.Where(s => s.LastName.Contains(searchString)
+                                       || s.FirstMidName.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    studentIQ = studentIQ.OrderByDescending(s => s.LastName);
+                    break;
+                case "Date":
+                    studentIQ = studentIQ.OrderBy(s => s.EnrollmentDate);
+                    break;
+                case "date_desc":
+                    studentIQ = studentIQ.OrderByDescending(s => s.EnrollmentDate);
+                    break;
+                default:
+                    studentIQ = studentIQ.OrderBy(s => s.LastName);
+                    break;
+            }
+
+            Student = await studentIQ.AsNoTracking().ToListAsync();
         }
     }
 }
